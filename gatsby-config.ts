919919913789385232a024da8f6640b85e5f0af4
Roy Tanaka <tanaka@mac.com>
2022-04-17 19:05:56 -0400

    config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,25 +1,40 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `denise-chan-design`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'Denise Chan',
+    titleTemplate: '%s · The Real Hero',
+    description: 'Senior Visual Designer',
+    url: 'https://www.denisechan.design',
+    // image: "/snape.jpg", // Path to the image placed in the 'static' folder, in the project's root directory.
+    instagramUrl: 'https://www.instagram.com/denisechandesign',
+    linkedinUrl: 'Senior Visual Designer',
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
     },
-    __key: "pages"
-  }]
+  ],
 };
 
 export default config;
