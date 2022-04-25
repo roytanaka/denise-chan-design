13e8f3708799e0339abfd64e5c5cf564d8f6f697
Roy Tanaka <tanaka@mac.com>
2022-04-25 08:09:10 -0400

    media queries

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,15 @@ const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   // only vars used are in build output
-  plugins: [postcssJitProps(OpenProps), postcssPresetEnv({ stage: 1 })],
+  plugins: [
+    postcssJitProps(OpenProps),
+    postcssPresetEnv({
+      stage: 1,
+      features: {
+        'custom-media-queries': {
+          importFrom: 'node_modules/open-props/media.min.css',
+        },
+      },
+    }),
+  ],
 };
