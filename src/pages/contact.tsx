3d56560082ca1f88e1950059fc69c61e765e3b31
Roy Tanaka <tanaka@mac.com>
2022-05-15 21:05:15 -0400

    contact from

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
 
 const Contact = () => {
   return (
     <Layout>
-      <div>Contact</div>
+      <Helmet title="Contact" />
+
+      <section className="container">
+        <h1>get in touch</h1>
+        <form className="form" name="contact" method="POST" data-netlify="true">
+          <div className="form-input">
+            <label htmlFor="fname">First name</label>
+            <input id="fname" type="text" name="fname" />
+          </div>
+          <div className="form-input">
+            <label htmlFor="lname">Last name</label>
+            <input id="lname" type="text" name="lname" />
+          </div>
+          <div className="form-input span-2">
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" name="email" />
+          </div>
+          <div className="form-input span-2">
+            <label htmlFor="message">Message</label>
+            <textarea rows={3} id="message" name="message"></textarea>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </section>
     </Layout>
   );
 };
